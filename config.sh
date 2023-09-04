35d5a3fd1551fd94ff325ebf3001b28ee49e18ee
Julak Bantur <137990782+bagusid93@users.noreply.github.com>
2023-09-05 02:55:39 +0800

    Update and rename cok.sh to config.sh

---
+++ config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Script By Julak Bantur
+#Hss-Punya
+###########- COLOR CODE -##############
+colornow=$(cat /etc/julak/theme/color.conf)
+NC="\e[0m"
+RED="\033[0;31m"
+COLOR1="$(cat /etc/julak/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+COLBG1="$(cat /etc/julak/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
+WH='\033[1;37m'
+###########- END COLOR CODE -##########
+
+clear
