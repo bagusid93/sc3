25a89b3dac00130ca23ce12a7b0e3b188f400e94
Julak Bantur <137990782+bagusid93@users.noreply.github.com>
2023-09-06 04:06:25 +0800

    Update user-lock.sh

--- ssh/user-lock.sh
+++ ssh/user-lock.sh
@@ -2,8 +2,13 @@
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 ###########- COLOR CODE -##############
+colornow=$(cat /etc/julak/theme/color.conf)
 NC="\e[0m"
-RED="\033[0;31m" 
+RED="\033[0;31m"
+COLOR1="$(cat /etc/julak/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
+COLBG1="$(cat /etc/julak/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
+WH='\033[1;37m'
+###########- END COLOR CODE -##########
 
 clear
 echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
