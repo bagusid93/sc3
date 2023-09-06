2e9a8240d7a62bfce1cd3314ca1cac0696fbe2ac
Julak Bantur <137990782+bagusid93@users.noreply.github.com>
2023-09-06 15:36:59 +0800

    Update miss.sh

--- miss.sh
+++ miss.sh
@@ -1,9 +1,34 @@
 clear
-echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "           Edit Limit Vmess          \E[0m"
-echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1 ${NC} ${COLBG1}                 ${WH}• MEMBER SSH •                ${NC}$COLOR1$NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo "USERNAME          EXP DATE          STATUS"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+while read expired
+do
+AKUN="$(echo $expired | cut -d: -f1)"
+ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
+exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+status="$(passwd -S $AKUN | awk '{print $2}' )"
+if [[ $ID -ge 1000 ]]; then
+if [[ "$status" = "L" ]]; then
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "LOCKED"
+else
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "UNLOCKED"
+fi
+fi
+done < /etc/passwd
+JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo "Account number: $JUMLAH user"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1${NC} ${COLBG1}              ${WH}• EDIT LIMIT SSH •                 ${NC}$COLOR1$NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
 echo ""
-grep -E "^#vm " "/etc/vmess/.vmess.db" | cut -d ' ' -f 2 | column -t | sort | uniq
+grep -E "^#ssh " "/etc/xray/ssh" | cut -d ' ' -f 2-3 | nl -s '
 echo ""
 echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 read -rp "Input Username : " user
@@ -11,7 +36,7 @@ if [ -z $user ]; then
 menu
 else
 read -p "Limit (IP): " ips
-echo -e "${ips}" > /etc/vmess/limit-ip/${user}
+echo -e "${ips}" > /etc/ssh/limit-ip/${user}
 clear
 echo -e "\033[0;35m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo " VMESS Account Was Successfully Edited"
