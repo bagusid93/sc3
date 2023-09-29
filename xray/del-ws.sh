9f2c2226aaeb8e70f74fe6489e5f2dde4d230b3a
Julak Bantur <137990782+bagusid93@users.noreply.github.com>
2023-09-29 15:29:49 +0800

    Update del-ws.sh

--- xray/del-ws.sh
+++ xray/del-ws.sh
@@ -16,49 +16,49 @@ WH='\033[1;37m'
 ###########- END COLOR CODE -##########
 
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^#vmg " "/etc/xray/config.json")
-	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
-		echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-                echo -e "$COLOR1 ${NC}${COLBG1}     ⇱ Delete  Account ⇲       ${NC} $COLOR1 $NC"
-      		echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-		echo ""
-		echo "You have no existing clients!"
-		echo ""
-		echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-		read -n 1 -s -r -p "Press any key to back on menu"
-        menu-vmess
-	fi
+    echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+    echo -e "$COLBG1        Member Xray/Vmess Account        $NC"
+    echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+    NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/vmess/.vmess.db")
+    if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
+        echo ""
+        echo "   You have no existing clients!"
+        echo ""
+        exit 0
+    fi
+    listmem=$(grep -s "^### " "/etc/vmess/.vmess.db" | cut -d ' ' -f 2-3 | column -t | nl)
+    clear
+    echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+    echo -e "$COLBG1        Member Xray/Vmess Account        $NC"
+    echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+    echo "    NO  USERNAME     EXPIRED"    
+    echo "    ------------------------"
+    echo "$listmem"
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+echo -e "$COLBG1            Delete vmess Account         $NC"
+echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━$NC"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '1' ]]; do
+    echo -e " "
+    read -rp "Input Username : " user
+    CLIENT_EXISTS=$(grep -w $user /etc/vmess/.vmess.db | wc -l)
 
-	clear
+    if [[ ${CLIENT_EXISTS} == '0' ]]; then
+        echo "No customer name available"
+    else
+        exp=$(grep -wE "^#vm $user" "/etc/xray/config.json" | cut -d ' ' -f 3 | sort | uniq)
+        sed -i "/^#vm $user $exp/,/^},{/d" /etc/xray/config.json
+        sed -i "/^#vmg $user $exp/,/^},{/d" /etc/xray/config.json
+        sed -i "/^### $user $exp/d" /etc/vmess/.vmess.db
+        rm /etc/xray/log-create-${user}.log
+        systemctl restart xray >/dev/null 2>&1
+        clear
         echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-        echo -e "$COLOR1 ${NC}${COLBG1}     ⇱ Delete  Account ⇲       ${NC} $COLOR1 $NC"
-      	echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-	echo " Select the existing client you want to remove"
-	echo " Press CTRL+C to return"
-	echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-	echo "     No  Expired   User"
-	grep -E "^#vmg " "/etc/xray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
-	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
-		if [[ ${CLIENT_NUMBER} == '1' ]]; then
-			read -rp "Select one client [1]: " CLIENT_NUMBER
-		else
-			read -rp "Select one client [1-${NUMBER_OF_CLIENTS}]: " CLIENT_NUMBER
-		fi
-	done
-user=$(grep -E "^#vmg " "/etc/xray/config.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^#vmg " "/etc/xray/config.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
-sed -i "/^#vmg $user $exp/,/^},{/d" /etc/xray/config.json
-sed -i "/^#vm $user $exp/,/^},{/d" /etc/xray/config.json
-sed -i "/^### $user $exp/d" /etc/vmess/.vmess.db
-rm /etc/xray/log-create-${user}.log
-systemctl restart xray > /dev/null 2>&1
-clear
-    echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-    echo " Vmess Account Deleted Successfully"
-    echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-    echo " Client Name : $user"
-    echo " Expired On  : $exp"
-    echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
-    echo ""
+        echo " Vmess Account Deleted Successfully"
+        echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+        echo " Client Name : $user"
+        echo " Expired On  : $exp"
+        echo -e "$COLOR1━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+        echo ""
+    fi
     read -n 1 -s -r -p "Press any key to back on menu"
     menu-vmess
