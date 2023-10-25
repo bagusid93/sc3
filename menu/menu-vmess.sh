b573a2884cd9e2aa4b751e541f2c081c4336df8f
Julak Bantur <137990782+bagusid93@users.noreply.github.com>
2023-10-25 14:45:31 +0800

    Update menu-vmess.sh

--- menu/menu-vmess.sh
+++ menu/menu-vmess.sh
@@ -599,33 +599,34 @@ systemctl restart xray > /dev/null 2>&1
 }
 
 function cek-vmess(){
+TIMES="10"
+CHATID=$(cat /etc/per/id)
+KEY=$(cat /etc/per/token)
+URL="https://api.telegram.org/bot$KEY/sendMessage"
+CITY=$(cat /etc/xray/city)
+domain=$(cat /etc/xray/domain)
+author=$(cat /etc/profil)
+#systemctl restart xray
+#sleep 1
 clear
-function con() {
-    local -i bytes=$1;
-    if [[ $bytes -lt 1024 ]]; then
-        echo "${bytes}B"
-    elif [[ $bytes -lt 1048576 ]]; then
-        echo "$(( (bytes + 1023)/1024 ))KB"
-    elif [[ $bytes -lt 1073741824 ]]; then
-        echo "$(( (bytes + 1048575)/1048576 ))MB"
-    else
-        echo "$(( (bytes + 1073741823)/1073741824 ))GB"
-    fi
-}
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '###' | cut -d ' ' -f 2 | sort | uniq`);
-echo -e "$COLOR1─────────────────────────────────────────$NC"
-echo -e "\e[42m        Vmess User Login Account         $NC"
-echo -e "$COLOR1─────────────────────────────────────────$NC"
+data=( `cat /etc/xray/config.json | grep '^#vm' | cut -d ' ' -f 2 | sort | uniq`);
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+echo -e "$COLOR1 ${NC} ${COLBG1}            ${WH}• USER ONLINE •                 ${NC} $COLOR1 $NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1┌─────────────────────────────────────────────────┐${NC}"
+
 for akun in "${data[@]}"
 do
 if [[ -z "$akun" ]]; then
 akun="tidakada"
 fi
+
 echo -n > /tmp/ipvmess.txt
 data2=( `cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq`);
 for ip in "${data2[@]}"
 do
+
 jum=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
 echo "$jum" >> /tmp/ipvmess.txt
@@ -635,30 +636,42 @@ fi
 jum2=$(cat /tmp/ipvmess.txt)
 sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
 done
+
 jum=$(cat /tmp/ipvmess.txt)
 if [[ -z "$jum" ]]; then
 echo > /dev/null
 else
-iplimit=$(cat /etc/julak/limit/vmess/ip/${akun})
 jum2=$(cat /tmp/ipvmess.txt | wc -l)
-byte=$(cat /etc/vmess/${akun})
-lim=$(con ${byte})
-wey=$(cat /etc/limit/vmess/${akun})
-gb=$(con ${wey})
-lastlogin=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 2 | tail -1)
-echo -e "$COLOR1─────────────────────────────────────────$NC"
-printf "  %-13s %-7s %-8s %2s\n" "  USERNAME : ${akun}"        | lolcat
-printf "  %-13s %-7s %-8s %2s\n" "  LOGIN    : $lastlogin"     | lolcat 
-printf "  %-13s %-7s %-8s %2s\n" "  QUOTA    : ${gb}"   | lolcat  
-printf "  %-13s %-7s %-8s %2s\n" "  LIMIT IP : $jum2/$iplimit" | lolcat;
-echo -e "$COLOR1─────────────────────────────────────────$NC"
-fi 
+echo -e "$COLOR1${NC} USERNAME : \033[0;33m$akun";
+echo -e "$COLOR1${NC} IP LOGIN : \033[0;33m$jum2";
+echo -e ""
+
+
+TEXT="
+<code>◇━━━━━━━━━━━━━━◇</code>
+<b>  ⚠️ XRAY VMESS NOTIF ⚠️</b>
+<b>         User Login</b>
+<code>◇━━━━━━━━━━━━━━◇</code>
+<b>DOMAIN    :</b> <code>${domain} </code>
+<b>USERNAME  :</b> <code>$akun </code>
+<b>TOTAL IP  :</b> <code>${jum2} </code>
+<code>◇━━━━━━━━━━━━━━◇</code>
+"
+
+curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
+
+fi
 rm -rf /tmp/ipvmess.txt
 done
+
 rm -rf /tmp/other.txt
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
+echo -e "$COLOR1┌────────────────────── ${WH}BY${NC} ${COLOR1}───────────────────────┐${NC}"
+echo -e "$COLOR1 ${NC}                  ${WH}• $author  •                   $COLOR1 $NC"
+echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 echo ""
-echo -e "$COLOR1─────────────────────────────────────────$NC"
-echo ""
+read -n 1 -s -r -p "   Press any key to back on menu"
+menu-vmess
 }
 function list-vmess(){
 clear
